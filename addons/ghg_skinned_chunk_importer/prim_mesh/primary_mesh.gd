eae542dccba69f22cf31f8e4564e6506141c694d
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2026-04-02 16:09:12 -0700

    use round like snapped on 0x04020001 to neat it up in 3 float notations

--- addons/ghg_skinned_chunk_importer/prim_mesh/primary_mesh.gd
+++ addons/ghg_skinned_chunk_importer/prim_mesh/primary_mesh.gd
@@ -3,15 +3,43 @@ extends EditorImportPlugin
 class_name PRIMARY_MESH_1a
 
 var root_node = Node3D.new()
+
+const SEEK_SET := 0
+const SEEK_CUR := 1
+const SEEK_END := 2
+
+
+func custom_seek(file: FileAccess, offset: int, whence: int) -> void:
+	var new_pos := 0
+	match whence:
+		SEEK_SET:
+			new_pos = offset
+		SEEK_CUR:
+			new_pos = file.get_position() + offset
+		SEEK_END:
+			new_pos = file.get_length() + offset
+		_:
+			push_error("Invalid whence value")
+			return
+	new_pos = clamp(new_pos, 0, file.get_length())
+	
+	file.seek(new_pos)
+
+func u8_to_s8(u8: int) -> int:
+	u8 &= 0xFF
+	if u8 & 0x80:
+		return u8 - 0x100
+	return u8
 		
 func u16_to_s16(u16: int) -> int:
 	u16 &= 0xFFFF
 	if u16 > 0x7FFF:
 		return u16 - 0x10000
 	return u16
-
 func _primary_mesh_(file : FileAccess, source_file : String):
 	file.seek(0)
+	
+	
 	
 	var FileSize = file.get_32()
 	var null01 = file.get_32()
@@ -28,113 +56,17 @@ func _primary_mesh_(file : FileAccess, source_file : String):
 	var namedtablesize1= file.get_32()
 	var namedtablelengh= file.get_32()
 	
+	var bna=[]
+	var stack=[]
+	
 	var boncount = Skeleton3D.new()
 	boncount.name = "GHG Armature"
 	root_node.add_child(boncount)
 	boncount.owner = root_node
+		
 	
-	file.seek(namedtablesize1)
+	var name_i=0
 	
-	var bbbint = 0
-	
-	var bbbbname = "%d" % bbbint
-	
-	var bobebane = file.get_buffer(namedtablelengh).get_string_from_utf8().split("0")
-	
-	file.seek(0)
-	
-	var BAdd=[]
-	var Bname=[]
-	
-	var matrixs=[]
-	
-	file.seek(bonesize1)
-	
-	for ij in range(BoneCount):
-		file.get_float()
-		file.get_float()
-		file.get_float()
-		file.get_float()
-		
-		file.get_float()
-		file.get_float()
-		file.get_float()
-		file.get_float()
-		
-		file.get_float()
-		file.get_float()
-		file.get_float()
-		file.get_float()
-		
-		file.get_float()
-		file.get_float()
-		file.get_float()
-		file.get_float()
-		
-		file.get_32()
-		file.get_32()
-		file.get_32()
-		
-		var offsetText = file.get_32()
-		var parent_id = file.get_8()
-		var name_offset = file.get_8()
-		
-		var parent_id2 = parent_id
-		
-		file.get_32()
-		file.get_32()
-		file.get_32()
-		file.get_16()
-		
-		BAdd.append(parent_id2)
-		Bname.append(bobebane)
-		
-		
-	file.seek(0)
-	file.seek(bonesize3)
-	
-	var idx=-1
-	
-	var ghgBones = []
-	var bone_id_name = "0"
-	var ccount = 0
-	
-	for iii in range(BoneCount):
-		var m1 = file.get_float()
-		var m2 = file.get_float()
-		var m3 = file.get_float()
-		var m4 = file.get_float()
-		
-		var m5 = file.get_float()
-		var m6 = file.get_float()
-		var m7 = file.get_float()
-		var m8 = file.get_float()
-		
-		var m9 = file.get_float()
-		var m10 = file.get_float()
-		var m11 = file.get_float()
-		var m12 = file.get_float()
-		
-		var m13 = file.get_float()
-		var m14 = file.get_float()
-		var m15 = file.get_float()
-		var m16 = file.get_float()
-		
-		var m1a = ([m1,m2,m3,m4])
-		var m2a = ([m5,m6,m7,m8])
-		var m3a = ([m9,m10,m11,m12])
-		var m4a = ([m13,m14,m15,m16])
-		matrixs.append_array([m1a,m2a,m3a,m4a])
-		
-		idx+=1
-		
-		boncount.add_bone(bone_id_name)
-		bone_id_name+="%d" % ccount
-		ccount+=1
-		boncount.get_bone_parent(BAdd[idx])
-		
-		boncount.set_bone_pose_position(idx,Vector3(m13,m14,-m15))
-		boncount.set_bone_pose_scale(idx, Vector3(m1,m6,m11))
 	file.seek(0)
 	
 	var faces4=[]
@@ -208,6 +140,13 @@ func _primary_mesh_(file : FileAccess, source_file : String):
 			var fbb=-2
 			var fcc=-1
 			
+			var faa1=-1
+			var fbb1=-4
+			var fcc1=-3
+			var fdd1=-2
+			
+			var faces1=[]
+			
 			
 			
 			
@@ -224,8 +163,6 @@ func _primary_mesh_(file : FileAccess, source_file : String):
 				if flgidx <= int(256):
 					if vertexCountb == int(3):
 						for j in range(vertexCountb):
-							#vertices[i] = Vector3(file.get_float(),file.get_float(),file.get_float())
-							#var nz = file.get_float()
 							var vx3 = file.get_16()
 							var vy3 = file.get_16()
 							var vz3 = file.get_16()
@@ -239,7 +176,6 @@ func _primary_mesh_(file : FileAccess, source_file : String):
 							var signed_vz: int = u16_to_s16(vz3)
 							
 							surf_tool3.add_vertex(Vector3(signed_vx,signed_vy,-signed_vz))
-							
 						for i in range(vertexCountb-2):
 							faa+=1*3
 							fbb+=1*3
@@ -248,6 +184,84 @@ func _primary_mesh_(file : FileAccess, source_file : String):
 							surf_tool3.add_index(faa)
 							surf_tool3.add_index(fbb)
 							surf_tool3.add_index(fcc)
+					elif vertexCountb == int(4):
+						for j in range(1):
+							var vx1 = file.get_16()
+							var vy1 = file.get_16()
+							var vz1 = file.get_16()
+							var nz1 = file.get_16()
+							var uvx1 = file.get_16()
+							var uvy1 = file.get_16()
+							var unkk = file.get_32()
+							
+							var vx2 = file.get_16()
+							var vy2 = file.get_16()
+							var vz2 = file.get_16()
+							var nz2 = file.get_16()
+							var uvx2 = file.get_16()
+							var uvy2 = file.get_16()
+							var unkk2 = file.get_32()
+							
+							var vx3 = file.get_16()
+							var vy3 = file.get_16()
+							var vz3 = file.get_16()
+							var nz3 = file.get_16()
+							var uvx3 = file.get_16()
+							var uvy3 = file.get_16()
+							var unkk3 = file.get_32()
+							
+							var vx4 = file.get_16()
+							var vy4 = file.get_16()
+							var vz4 = file.get_16()
+							var nz4 = file.get_16()
+							var uvx4 = file.get_16()
+							var uvy4 = file.get_16()
+							var unkk4 = file.get_32()
+							
+							var signed_vx1: int = u16_to_s16(vx1)
+							var signed_vy1: int = u16_to_s16(vy1)
+							var signed_vz1: int = u16_to_s16(vz1)
+							var signed_vx2: int = u16_to_s16(vx2)
+							var signed_vy2: int = u16_to_s16(vy2)
+							var signed_vz2: int = u16_to_s16(vz2)
+							var signed_vx3: int = u16_to_s16(vx3)
+							var signed_vy3: int = u16_to_s16(vy3)
+							var signed_vz3: int = u16_to_s16(vz3)
+							var signed_vx4: int = u16_to_s16(vx4)
+							var signed_vy4: int = u16_to_s16(vy4)
+							var signed_vz4: int = u16_to_s16(vz4)
+							
+							
+							if vy3 == vy4:
+								surf_tool3.add_vertex(Vector3(float(signed_vx1)/4096,float(signed_vy1/4096),float(-signed_vz1/4096)))
+								surf_tool3.add_vertex(Vector3(float(signed_vx2)/4096,float(signed_vy2/4096),float(-signed_vz2/4096)))
+								surf_tool3.add_vertex(Vector3(float(signed_vx3)/4096,float(signed_vy3/4096),float(-signed_vz3/4096)))
+								surf_tool3.add_vertex(Vector3(float(signed_vx4)/4096,float(signed_vy4/4096),float(-signed_vz4/4096)))
+								for i in range(vertexCountb-3):
+									faa1+=1*4
+									fbb1+=1*4
+									fcc1+=1*4
+									fdd1+=1*4
+									surf_tool3.add_index(faa1)
+									surf_tool3.add_index(fbb1)
+									surf_tool3.add_index(fcc1)
+									surf_tool3.add_index(fbb1)
+									surf_tool3.add_index(fcc1)
+									surf_tool3.add_index(fdd1)
+							elif vx2 == vy3:
+								pass
+								
+							elif vx3 == vy4:
+								pass
+								
+							elif vy2 == vy3:
+								pass
+							
+							
+						
+					
+							
+						
 				surf_tool3.generate_normals(true)
 				surf_tool3.set_material(mat03)
 			array_mesh3 = surf_tool3.commit(array_mesh3)
@@ -343,72 +357,82 @@ func _primary_mesh_(file : FileAccess, source_file : String):
 			var vertexCounta = file.get_8()/2
 			var flagsa1 = file.get_8()
 			
-			
-			var faa=-1
-			var fbb=0
-			var fcc=1
+			if vertexCounta:
 			
 			
-			
-			
-			vertices2.resize(vertexCounta)
-			faces2.resize(vertexCounta)
-			uvs2.resize(vertexCounta)
-			normals2.resize(vertexCounta)
-			surf_tool2.begin(Mesh.PRIMITIVE_TRIANGLES)
-			
-			#var faces = [[0,1,2]]
-			if flagsa1 == int(108):
-				flgidx+=1
+				var faa=-1
+				var fbb=0
+				var fcc=1
 				
-				if flgidx <= int(256):
-					for j in range(vertexCounta):
-						#vertices[i] = Vector3(file.get_float(),file.get_float(),file.get_float())
-						#var nz = file.get_float()
-						var vx2 = file.get_float()
-						var vy2 = file.get_float()
-						var vz2 = file.get_float()
-						var brightness = file.get_float()
-						var uvx2 = file.get_float()
-						var uvy2 = file.get_float()
-						var unkk = file.get_float()
-						var type4 = file.get_8()
-						var value1 = file.get_8()
-						var normalZ = file.get_16()
-						
-						var vx1 = vx2
-						var vy1 = vy2
-						var vz1 = vz2
-						
-						if vx2 >= float(10000) and vy2 >= float(10000) and vz2 >= float(10000) or vx2 <= float(-10000) and vy2 <= float(-10000) and vz2 <= float(-10000):
-							vx1-=vx1
-							vy1-=vy1
-							vz1-=vz1
-						surf_tool2.set_color(Color(float(brightness),float(brightness),float(brightness),float(1)))
-						surf_tool2.set_uv(Vector2(uvx2,-uvy2))
-						surf_tool2.add_vertex(Vector3(vx2,vy2,-vz2))
-						var type5 = type4==0
-						faa+=1
-						fbb+=1
-						fcc+=1
-						if type5:
-							faces4a=j+j+type4-type4-1+faa-j-j-1+j%2
-							faces5a=j-j+type4-type4+1+fbb-2-1+j-j-j%2
-							faces6a=j+type4-type4+fcc-j+2-4
-							var a12=faces4a
-							var b12=faces5a
-							var c12=faces6a
-	
+				
+				
+				
+				vertices2.resize(vertexCounta)
+				faces2.resize(vertexCounta)
+				uvs2.resize(vertexCounta)
+				normals2.resize(vertexCounta)
+				
+				
+				surf_tool2.begin(Mesh.PRIMITIVE_TRIANGLES)
+				
+				#var faces = [[0,1,2]]
+				if flagsa1 == int(108):
+					flgidx+=1
+					
+					if flgidx <= int(256):
+						for j in range(vertexCounta):
+							#vertices[i] = Vector3(file.get_float(),file.get_float(),file.get_float())
+							#var nz = file.get_float()
+							var vx2 = file.get_float()
+							var vy2 = file.get_float()
+							var vz2 = file.get_float()
+							var brightness = file.get_float()
+							var uvx2 = file.get_float()
+							var uvy2 = file.get_float()
+							var unkk = file.get_float()
+							var type4 = file.get_8()
+							var value1 = file.get_8()
+							var normalZ = file.get_16()
+							
+							var vx1 = vx2
+							var vy1 = vy2
+							var vz1 = vz2
+							
+							var vx2a = snapped(vx2,0.001)
+							var vy2a = snapped(vy2,0.001)
+							var vz2a = snapped(vz2,0.001)
+							
+							if vx2 >= float(10000) and vy2 >= float(10000) and vz2 >= float(10000) or vx2 <= float(-10000) and vy2 <= float(-10000) and vz2 <= float(-10000):
+								vx1-=vx1
+								vy1-=vy1
+								vz1-=vz1
+							surf_tool2.set_color(Color(float(brightness),float(brightness),float(brightness),float(1)))
+							surf_tool2.set_uv(Vector2(uvx2,-uvy2))
+							surf_tool2.add_vertex(Vector3(vx2a,vy2a,-vz2a))
+							var type5 = type4==0
+							faa+=1
+							fbb+=1
+							fcc+=1
+							if type5:
+								faces4a=j+j+type4-type4-1+faa-j-j-1+j%2
+								faces5a=j-j+type4-type4+1+fbb-2-1+j-j-j%2
+								faces6a=j+type4-type4+fcc-j+2-4
+								var a12=faces4a
+								var b12=faces5a
+								var c12=faces6a
+		
 
-							surf_tool2.add_index(a12)
-							surf_tool2.add_index(b12)
-							surf_tool2.add_index(c12)
+								surf_tool2.add_index(a12)
+								surf_tool2.add_index(b12)
+								surf_tool2.add_index(c12)
+								
+						surf_tool2.generate_normals(true)
+						surf_tool2.set_material(mat02)
+						array_mesh2 = surf_tool2.commit(array_mesh2)
 				
-				surf_tool2.generate_normals(true)
-				surf_tool2.set_material(mat02)
-			array_mesh2 = surf_tool2.commit(array_mesh2)
+					
 			#array_mesh3 = surf_tool3.commit(array_mesh3)
-			
+				
 	immeediate_mesh2_.mesh = array_mesh2
 	immeediate_mesh2_.name = "0x04020001 ghg 3"
 	root_node.add_child(immeediate_mesh2_)
